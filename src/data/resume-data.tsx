e98757736f0096129718b399575418d744463534
BlameMe9 <110724383+BlameMe9@users.noreply.github.com>
2025-02-17 19:49:04 +0100

    Update resume-data.tsx

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -28,7 +28,7 @@ export const RESUME_DATA = {
   about:
     "",
 summary: [
-  "PhD candidate and leadership consultant with experience as a business owner, army team leader, and student supervisor. Skilled at setting goals, removing obstacles, and driving strategic decisions in collaborative environments. Recognized for hands-on leadership and continuous improvement, I aim to leverage my AI expertise and background in organizational psychology to drive innovation."
+  "PhD candidate and leadership consultant with experience as a business owner, army team leader, and student supervisor. Skilled at setting goals, removing obstacles, and driving strategic decisions in collaborative environments. Recognized for hands-on leadership and continuous improvement, I aim to leverage my background in organizational psychology and AI expertise to drive innovation."
 ] as const,
 
   avatarUrl: "https://media.licdn.com/dms/image/v2/D4D03AQGnEDQokH4eHA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1712494802583?e=1744848000&v=beta&t=HYiJoO4OI39uYECXIJlR9oPrTh77NOc7okkWwSgAE_8",
