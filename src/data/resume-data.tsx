25f09e2047e4afbb1e1f4720a5515f33be0d8e81
BlameMe9 <110724383+BlameMe9@users.noreply.github.com>
2025-02-17 19:57:02 +0100

    Update resume-data.tsx

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -95,7 +95,7 @@ summary: [
       start: "2021",
       end: "Current",
       description:
-        "Østlyng & Bjerke specializes in leadership and organizational development. I led the development and sales of digital courses and the company's website, facilitated leadership programs, and developed insight analyses using qualitative and quantitative data",
+        "Ø&B specializes in leadership and organizational development. I led the development and sales of digital courses and the company's website, facilitated leadership programs, and developed insight analyses",
     },
     {
       company: "VRID",
